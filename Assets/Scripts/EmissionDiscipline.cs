4e16628c302af8c4e047b5da2a2abdc2bfe2117e
Nathan Knebusch <nateknebusch@gmail.com>
2019-08-29 17:37:47 -0700

    added CCTV

---
+++ Assets/Scripts/EmissionDiscipline.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissionDiscipline : MonoBehaviour
+{
+    public ParticleSystem emission;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(emission.isPlaying)
+        {
+            emission.Stop();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(emission == null)
+        {
+            return;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            emission.Play();
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            emission.Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/RenderTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTarget : MonoBehaviour
+{
+    public RenderTexture rt;
+    public GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Graphics.SetRenderTarget(rt, 0, CubemapFace.Unknown, 0);
+    }
+}
